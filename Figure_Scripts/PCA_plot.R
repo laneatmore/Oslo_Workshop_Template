76ffd31331df190e71f57e142848a6206c349a95
laneatmore <52425233+laneatmore@users.noreply.github.com>
2021-10-08 07:47:54 -0500

    Show file input options, add comments

--- Figure_Scripts/PCA_plot.R
+++ Figure_Scripts/PCA_plot.R
@@ -1,18 +1,25 @@
 library(ggplot2)
 
 setwd('~/Documents/Oslo_Workshop/')
-eigenvec_table <- read.table("West_East.pca.evec")
 
+#Read in the eigenvec_table from WRAPsnps.sh
+eigenvec_table <- read.table(<MY.pca.evec>)
+
+#open a PDF file to print out the PCA
 pdf('PCA_BAMscorer.pdf')
+
+#create the PCA plot with headers and labels
 eigenvec_plot <- ggplot(eigenvec_table, aes(x = V2, y = V3)) + 
                     geom_point(size = 2) +
                     labs(title = "PCA",
                          x = "PC1", y = "PC2") +
                     theme_bw() +
                     theme(plot.title = element_text(hjust=0.5))
- 
+
+#visualize the plot
 eigenvec_plot
 
+#close the PDF
 dev.off()
                     
                  
